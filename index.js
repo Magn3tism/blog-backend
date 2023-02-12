da9763e148c06cf95c5a660fbb056c62b869ceea
Magn3tism <chapagainsakshyam@gmail.com>
2023-02-12 13:34:42 +0545

    Refactor controller

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
 
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blog");
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response, next) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((err) => next(err));
-});
+app.use("/api/blogs", blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
