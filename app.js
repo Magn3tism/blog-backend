00ffd9aaa9322aedf450b257badcbc464ca819c7
Magn3tism <chapagainsakshyam@gmail.com>
2023-02-12 13:45:11 +0545

    Add middlewares

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const blogsRouter = require("./controllers/blog");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
+const middleware = require("./utils/middleware");
 
 mongoose.set("strictQuery", true);
 
@@ -17,7 +18,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
