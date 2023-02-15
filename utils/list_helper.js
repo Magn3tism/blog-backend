eca2e9b93251685dc624a496bd0ae266d3dfe086
Magn3tism <chapagainsakshyam@gmail.com>
2023-02-15 12:31:41 +0545

    Add test for most likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((likes, blog) => likes + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return "empty list";
+  if (blogs.length === 1) return blogs[0];
+
+  let fav = blogs[0];
+
+  blogs.forEach((blog) => {
+    if (blog.likes > fav.likes) fav = blog;
+  });
+
+  return fav;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
